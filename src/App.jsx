5df8b3c1b84fc5c3d38840d5161afa122fdf1bfe
mario silva <msilvasaquiray17@gmail.com>
2025-09-14 23:45:42 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1>Tienda de Repuesto de Autos: Silva Saquiray Mario</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
